cc64a52dd3fa1ec2854a83135f581627285b6ec7
Brian Smith <hoshiningen@gmail.com>
2018-02-18 11:48:56 -0500

    I think so..

--- benchmarks/bm_stack.h
+++ benchmarks/bm_stack.h
@@ -1,24 +1,24 @@
 #pragma once
 
-//#include "benchmark/benchmark.h"
-//
-//class StackFixture : public benchmark::Fixture {};
-//
-//BENCHMARK_F(StackFixture, PushTest)(benchmark::State& st)
-//{
-//    int i = 0;
-//    for (auto _ : st) {
-//        i++;
-//    }
-//}
-//
-//BENCHMARK_F(StackFixture, PopTest)(benchmark::State& st)
-//{
-//    int i = 0;
-//    for (auto _ : st) {
-//        i--;
-//    }
-//}
-//
-//BENCHMARK_REGISTER_F(StackFixture, PushTest)->Threads(4);
-//BENCHMARK_REGISTER_F(StackFixture, PopTest)->Threads(4);
+#include "benchmark/benchmark.h"
+
+class StackFixture : public benchmark::Fixture {};
+
+BENCHMARK_F(StackFixture, PushTest)(benchmark::State& st)
+{
+    int i = 0;
+    for (auto _ : st) {
+        i++;
+    }
+}
+
+BENCHMARK_F(StackFixture, PopTest)(benchmark::State& st)
+{
+    int i = 0;
+    for (auto _ : st) {
+        i--;
+    }
+}
+
+BENCHMARK_REGISTER_F(StackFixture, PushTest)->Threads(4);
+BENCHMARK_REGISTER_F(StackFixture, PopTest)->Threads(4);
